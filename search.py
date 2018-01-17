e301a6a3cdcdd4595e12dd0433fd21e7c690a8a8
mdihcroe <mdihcroe@gmail.com>
2018-01-17 15:22:01 +0300

    comments

--- search.py
+++ search.py
@@ -5,10 +5,13 @@ Entrez.email = "elizarova@phystech.edu"
 
 key_words = '[[ [ seawater OR marine OR sediment OR freshwater OR ocean OR river OR lake OR aquatic] metagenome ] ' \
             'OR [ marine microbial community ]] NOT gut] NOT intestine] NOT human'
+
+# get number of results
 handle = Entrez.esearch(db="bioproject", term=key_words)
 record = Entrez.read(handle)
 handle.close()
 
+# retrieve all results
 handle = Entrez.esearch(db="bioproject", term=key_words, retmax=record['Count'])
 record = Entrez.read(handle)
 handle.close()
@@ -17,24 +20,26 @@ number_of_projects = len(record['IdList'])
 print 'Number of projects to be checked: ', number_of_projects
 
 for id in record['IdList']:
+    # get bioproject id
     handle = Entrez.efetch(db="bioproject", id=id, retmode='xml')
     record = handle.read()
     handle.close()
     root = ET.fromstring(record)
-
     project_accession = root.findall('.//ArchiveID')[0].attrib['accession']
-    # print project_accession
-    handle = Entrez.esearch(db="pmc", term=project_accession)
+
+    # check if related to any article in db_name
+    db_name = "pmc"
+    handle = Entrez.esearch(db=db_name, term=project_accession)
     record = handle.read()
     handle.close()
-
     root = ET.fromstring(record)
     number_of_articles = int(root[0].text)
+
     if number_of_articles != 0:
         id_article = root.findall('.//IdList/Id')[0].text
         print 'ARTICLE FOUND:', id_article
 
-        db_name = "pmc"
+        # get article info
         handle = Entrez.efetch(db=db_name, id=id_article, retmode='xml')
         record = handle.read()
         handle.close()
