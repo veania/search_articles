bf4d008587bb524151fc0f99b16ad771807c447c
mdihcroe <mdihcroe@gmail.com>
2018-01-18 01:38:04 +0300

    tries, sleep, save checked projects to file, check again only if not checked yet

--- search.py
+++ search.py
@@ -1,68 +1,119 @@
 from Bio import Entrez
 import xml.etree.ElementTree as ET
+import time
+from pathlib import Path
+import urllib2
 
-Entrez.email = "elizarova@phystech.edu"
 
-key_words = '[[ [ seawater OR marine OR sediment OR freshwater OR ocean OR river OR lake OR aquatic] metagenome ] ' \
-            'OR [ marine microbial community ]] NOT gut] NOT intestine] NOT human'
+def not_checked(id):
+    hist_file = 'ids_checked.txt'
+    if Path(hist_file).is_file() and id not in open(hist_file).read():
+        return True
+    else:
+        return False
 
-# get number of results
-handle = Entrez.esearch(db="bioproject", term=key_words)
-record = Entrez.read(handle)
-handle.close()
 
-# retrieve all results
-handle = Entrez.esearch(db="bioproject", term=key_words, retmax=record['Count'])
-record = Entrez.read(handle)
-handle.close()
+def main():
+    Entrez.email = "elizarova@phystech.edu"
 
-number_of_projects = len(record['IdList'])
-print 'Number of projects to be checked: ', number_of_projects
+    key_words = '[[ [ seawater OR marine OR sediment OR freshwater OR ocean OR river OR lake OR aquatic] metagenome ] ' \
+                'OR [ marine microbial community ]] NOT gut] NOT intestine] NOT human'
 
-for id in record['IdList']:
-    print 'Bioproject id: ', id
+    # get number of results
 
-    # get bioproject id
-    handle = Entrez.efetch(db="bioproject", id=id, retmode='xml')
-    record = handle.read()
+    handle = Entrez.esearch(db="bioproject", term=key_words)
+    record = Entrez.read(handle)
     handle.close()
-    root = ET.fromstring(record)
-    project_accession = root.findall('.//ArchiveID')[0].attrib['accession']
-    print 'Bioproject accession number: ', project_accession
 
-    # check if related to any article in db_name
-    db_name = "pmc"
-    handle = Entrez.esearch(db=db_name, term=project_accession)
-    record = handle.read()
+    # retrieve all results
+    handle = Entrez.esearch(db="bioproject", term=key_words, retmax=record['Count'])
+    record = Entrez.read(handle)
     handle.close()
-    root = ET.fromstring(record)
-    number_of_articles = int(root[0].text)
 
-    if number_of_articles != 0:
-        id_article = root.findall('.//IdList/Id')[0].text
-        print 'ARTICLE FOUND:', id_article
+    number_of_projects = len(record['IdList'])
+    print 'Number of projects to be checked: ', number_of_projects
 
-        # get article info
-        handle = Entrez.efetch(db=db_name, id=id_article, retmode='xml')
-        record = handle.read()
-        handle.close()
-        root = ET.fromstring(record)
+    for id in record['IdList']:
+        print 'Bioproject id: ', id
 
-        try:
-            title = root.findall('.//title-group/article-title')[0].text
-            abstract = root.findall('.//abstract/p')[0].text
+        if not_checked(id):
+            # get bioproject id
+            try:
+                handle = Entrez.efetch(db="bioproject", id=id, retmode='xml')
+                record = handle.read()
+                handle.close()
 
-            txt = open("Output.txt", "a")
-            txt.write('%s\n%s id: %s\nTITLE: %s\nABSTRACT:\n%s\n\n' %
-                      (project_accession, db_name, id_article, title, abstract))
-            txt.close()
+            except urllib2.HTTPError or urllib2.URLError:
+                print 'error'
+                txt = open("urllib2Error", "a")
+                txt.write('prj_id: %s\n', id)
+                txt.close()
+                continue
 
-        except UnicodeEncodeError:
-            txt = open("UnicodeEncodeError", "a")
-            txt.write('%s\n', id_article)
-            txt.close()
+            root = ET.fromstring(record)
+            project_accession = root.findall('.//ArchiveID')[0].attrib['accession']
+            print 'Bioproject accession number: ', project_accession
 
-    number_of_projects -= 1
-    print 'Number of project to be checked left: ', number_of_projects
-    print '_'
+            # check if related to any article in db_name
+            db_name = "pmc"
+            try:
+                handle = Entrez.esearch(db=db_name, term=project_accession)
+                record = handle.read()
+                handle.close()
 
+            except urllib2.HTTPError or urllib2.URLError:
+                print 'error'
+                txt = open("urllib2Error", "a")
+                txt.write('prj: %s\n', project_accession)
+                txt.close()
+                continue
+
+            root = ET.fromstring(record)
+            number_of_articles = int(root[0].text)
+
+            if number_of_articles != 0:
+                id_article = root.findall('.//IdList/Id')[0].text
+                print 'ARTICLE FOUND:', id_article
+
+                # get article info
+                try:
+                    handle = Entrez.efetch(db=db_name, id=id_article, retmode='xml')
+                    record = handle.read()
+                    handle.close()
+                except urllib2.HTTPError or urllib2.URLError:
+                    print 'error'
+                    txt = open("urllib2Error", "a")
+                    txt.write('id_art: %s\n', id_article)
+                    txt.close()
+                    continue
+
+                root = ET.fromstring(record)
+
+                try:
+                    title = root.findall('.//title-group/article-title')[0].text
+                    abstract = root.findall('.//abstract/p')[0].text
+
+                    txt = open("Output.txt", "a")
+                    txt.write('%s\n%s id: %s\nTITLE: %s\nABSTRACT:\n%s\n\n' %
+                              (project_accession, db_name, id_article, title, abstract))
+                    txt.close()
+
+                except UnicodeEncodeError:
+                    print 'error'
+                    txt = open("UnicodeEncodeError", "a")
+                    txt.write('%s\n', id_article)
+                    txt.close()
+
+            f = open('ids_checked.txt', 'a')
+            f.write('%s\n' % id)
+            f.close()
+
+        number_of_projects -= 1
+        print 'Number of project to be checked left: ', number_of_projects
+        print '_'
+
+        time.sleep(5)
+
+
+if __name__ == "__main__":
+    main()
